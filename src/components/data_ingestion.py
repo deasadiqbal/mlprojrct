0e3dc62cdfa5e714ff5d2f68bfe4526e7f845278
asad <asad95298@gmail.com>
2023-04-05 00:32:44 +0500

    data model trainer

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -11,6 +11,8 @@ from dataclasses import dataclass
 
 from src.components.data_transformation import DataTransformation
 from src.components.data_transformation import DataTransformationConfig
+from src.components.model_training import ModelTrainer
+from src.components.model_training import ModelTrainerConfig
 
 @dataclass
 class DataIngestionConfig:
@@ -52,8 +54,10 @@ if __name__ == '__main__':
     obj = DataIngestion(config)
     train_data, test_data = obj.initate_data_ingestion()
     data_transformation = DataTransformation()
-    data_transformation.initiate_data_transformation(train_data,test_data)
+    train_arr,test_arr,_ = data_transformation.initiate_data_transformation(train_data,test_data)
 
+    model_trainer = ModelTrainer()
+    print(model_trainer.initiate_model_trainer(train_arr, test_arr))
 
 
 
--- src/utils.py
+++ src/utils.py
@@ -4,6 +4,7 @@ import pandas as pd
 import os
 from src.exception import ExceptionHandler
 import dill
+from sklearn.metrics import r2_score
 
 def save_object(file_path, obj):
     try:
@@ -14,3 +15,19 @@ def save_object(file_path, obj):
             dill.dump(obj, f)
     except Exception as e:
         raise ExceptionHandler(e,sys)
+
+def evaluate_model(X_train, y_train, X_test, y_test, models):
+    model_report = {}
+    try:
+        for i, model_name in enumerate(models):
+            model = models[model_name]
+            model.fit(X_train, y_train)
+            y_pred = model.predict(X_test)
+            r2 = r2_score(y_test, y_pred)
+            model_report[model_name] = r2
+
+    except Exception as e:
+        logging.error(e)
+        raise ExceptionHandler(e)
+
+    return model_report
